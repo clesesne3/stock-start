4bae476f4649c0462ca47cb37e3c4907ae35221b
Chris Lesesne <clesesne3@gmail.com>
2018-02-18 12:13:50 -0800

    added server.js and npm dependencies; edit content sections

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const pg = require('pg');
+const cors = require('cors');
+const superAgent = require('superagent');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+
+// test route
+app.get('/', (req, res) => {
+  res.send('Route Successful');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server istening on PORT ${PORT}`)
+});
